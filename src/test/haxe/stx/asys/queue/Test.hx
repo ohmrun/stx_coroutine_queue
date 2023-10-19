bf1b5e56429880d4b608ff5f5092d58cc69ccf64
0b1kn00b <polysemantic@gmail.com>
2023-10-19 12:52:20 +0200

    initial

---
+++ src/test/haxe/stx/asys/queue/Test.hx
@@ -0,0 +1,18 @@
+package stx.asys.queue;
+
+using stx.Nano;
+using stx.Test;
+using stx.Log;
+
+class Test extends TestCase{
+  static public function main(){
+    __.test().run(
+      [
+        new Test()
+      ],[]
+    );
+  }
+  public function test(){
+    
+  }
+}
